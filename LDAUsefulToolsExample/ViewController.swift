d13bc7772a0b63ee0874f0702195934db7e13ff9
POLYAKOV Lev <Lev.POLYAKOV@raiffeisen.ru>
2019-11-28 12:47:01 +0300

    init

---
+++ LDAUsefulToolsExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LDAUsefulToolsExample
+//
+//  Created by POLYAKOV Lev on 28/11/2019.
+//  Copyright © 2019 POLYAKOV Lev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
